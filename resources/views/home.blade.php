954856047f66e9d6c24f9f81c8c0652e3d601082
usman299 <usman@codingcrust.com>
2022-02-25 05:10:23 -0800

    done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,41 +3,42 @@
 
             <div class="row clearfix">
                 <div class="col-lg-3 col-md-6">
-                    <div class="card text-center">
-                        <div class="body">
-                            <p class="m-b-20"><i class="zmdi zmdi-balance zmdi-hc-3x col-amber"></i></p>
-                            <span>Revenu total</span>
-                            <h3 class="m-b-10"><span class="number count-to" data-from="0" data-to="2078" data-speed="2000" data-fresh-interval="700">2078</span>€</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="card text-center">
-                        <div class="body">
-                            <p class="m-b-20"><i class="zmdi zmdi-assignment zmdi-hc-3x col-blue"></i></p>
-                            <span>Total des commandes</span>
-                            <h3 class="m-b-10 number count-to" data-from="0" data-to="865" data-speed="2000" data-fresh-interval="700">865</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="card text-center">
-                        <div class="body">
-                            <p class="m-b-20"><i class="zmdi zmdi-shopping-basket zmdi-hc-3x"></i></p>
-                            <span>Produits totaux</span>
-                            <h3 class="m-b-10 number count-to" data-from="0" data-to="3502" data-speed="2000" data-fresh-interval="700">3502</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="card text-center">
-                        <div class="body">
-                            <p class="m-b-20"><i class="zmdi zmdi-account-box zmdi-hc-3x col-green"></i></p>
-                            <span>Total praticien</span>
-                            <h3 class="m-b-10 number count-to" data-from="0" data-to="78" data-speed="2000" data-fresh-interval="700">78</h3>
-                        </div>
-                    </div>
-                </div>
+
+{{--                    <div class="card text-center">--}}
+{{--                        <div class="body">--}}
+{{--                            <p class="m-b-20"><i class="zmdi zmdi-balance zmdi-hc-3x col-amber"></i></p>--}}
+{{--                            <span>Revenu total</span>--}}
+{{--                            <h3 class="m-b-10"><span class="number count-to" data-from="0" data-to="2078" data-speed="2000" data-fresh-interval="700">2078</span>€</h3>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-3 col-md-6">--}}
+{{--                    <div class="card text-center">--}}
+{{--                        <div class="body">--}}
+{{--                            <p class="m-b-20"><i class="zmdi zmdi-assignment zmdi-hc-3x col-blue"></i></p>--}}
+{{--                            <span>Total des commandes</span>--}}
+{{--                            <h3 class="m-b-10 number count-to" data-from="0" data-to="865" data-speed="2000" data-fresh-interval="700">865</h3>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-3 col-md-6">--}}
+{{--                    <div class="card text-center">--}}
+{{--                        <div class="body">--}}
+{{--                            <p class="m-b-20"><i class="zmdi zmdi-shopping-basket zmdi-hc-3x"></i></p>--}}
+{{--                            <span>Produits totaux</span>--}}
+{{--                            <h3 class="m-b-10 number count-to" data-from="0" data-to="3502" data-speed="2000" data-fresh-interval="700">3502</h3>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-3 col-md-6">--}}
+{{--                    <div class="card text-center">--}}
+{{--                        <div class="body">--}}
+{{--                            <p class="m-b-20"><i class="zmdi zmdi-account-box zmdi-hc-3x col-green"></i></p>--}}
+{{--                            <span>Total praticien</span>--}}
+{{--                            <h3 class="m-b-10 number count-to" data-from="0" data-to="78" data-speed="2000" data-fresh-interval="700">78</h3>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
                 <div class="col-lg-3 col-md-6">
                     <div class="card text-center">
                         <div class="body">
